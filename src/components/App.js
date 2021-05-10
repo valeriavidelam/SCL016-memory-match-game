176f270453ed4eb311409ff085bc2ba3cc4a52bc
Valeria Videla <valeravidela@MacBook-Pro-de-Valera.local>
2021-05-10 10:49:32 -0400

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,69 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
-//
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-// fetch('./data/pokemon/pokemon.json')
-//   .then(resp => resp.json())
-//   .then(console.log)
-//   .catch(console.error);
-//
+//Import data from .js file
+import pokemon from '../data/pokemon/pokemon.js'; 
+//console.log(pokemon);
+
+// array to show the pokemon in items 
+const items = pokemon.items;  
+//console.log(items);
+//expected output: 9
+
+// new array to show pokemon's items again
+const items2 = pokemon.items; 
+//console.log(items2);
+//expected output: 9
+
+//concat both array to multiplicate the pokemon data
+const allItems = items.concat (items2); 
+console.log(allItems);
+//expected output: 18
+
+//call the function 
+shuffle(allItems);
+//console.log(shuffle);
+
+const pokemonContainer = document.getElementById('cardContainer');
+export let showPokemon = (allItems) => {
+  let dataPokemon = '';
+  allItems.map((obj) => {
+    dataPokemon += `   <div class = "pokemonCard">
+    <div class = "internalCard">
+    <img src='${obj.image}' alt = "Image" class="imagePokemon"/>
+    </div>
+    </div>
+    `
+  });
+// return the new array with the data
+ return dataPokemon; 
+}
+// show the data in the Html with the Div that I create before.
+  pokemonContainer.innerHTML = showPokemon(allItems); 
+
+//Trying to randomize the order of the game
+function shuffle(allItems) {
+  let m = allItems.length, t, i;
+
+// While there remain elements to shuffle
+  while (m) {
+
+  // Pick a remaining element
+  i = Math.floor(Math.random() * m--);
+
+  // And swap it with the current element.
+  t = allItems[m];
+  allItems[m] = allItems[i];
+  allItems[i] = t;
+}
+
+return allItems;
+}  
 
 const App = () => {
   const el = document.createElement('div');
-
   el.className = 'App';
-  el.textContent = 'Hola mundo!';
 
+
+
+    
   return el;
 };
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,42 @@
 import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
+
+var gameCardsContainer = document.querySelector('#gameCards');
+var resetButton = document.querySelector('#reset-button');
+var modal = document.querySelector('.modal-container');
+var modalText = document.querySelector('.modal-text');
+
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardClasses = null;
+var secondCardClasses = null;
+
+var maxMatches = 9;
+var matches = 0;
+
+var attempts = 0;
+var gamesPlayed = 0;
+
+// Dynamic Card Shuffle
+
+var cardFrontDeck = [
+    'card-front-1',
+    'card-front-2',
+    'card-front-3',
+    'card-front-4',
+    'card-front-5',
+    'card-front-6',
+    'card-front-7',
+    'card-front-8',
+    'card-front-9',
+    'card-front-1',
+    'card-front-2',
+    'card-front-3',
+    'card-front-4',
+    'card-front-5',
+    'card-front-6',
+    'card-front-7',
+    'card-front-8',
+    'card-front-9'
+]
